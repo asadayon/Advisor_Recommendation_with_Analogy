d0831ce0f17d3b0186ce31dbcc92d51af4d151bb
Md Ashaduzzaman <102450075+asadayon@users.noreply.github.com>
2026-07-30 15:46:23 -0400

    Update instructions for selecting research keywords

--- grad_adv_rec.py
+++ grad_adv_rec.py
@@ -1922,7 +1922,7 @@ def render_recommender_page(version, scenario_index):
     st.info(scenario)
 
     st.markdown(
-        "Enter keywords of your research interest to get the system's recommendations. Select **all and only the CAPITALIZED BOLD KEYWORDS** from the list. Do not select any additional keywords or leave any bold keyword unselected."
+        "Enter keywords of your research interest to get the system's recommendations. Select the **CAPITALIZED BOLD KEYWORDS** from the list. Do not select any additional keywords or leave any bold keyword unselected."
     )
 
     keywords = st.multiselect(
